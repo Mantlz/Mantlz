5a5cada12a3247093a1b0eaa3863d9de64fcb922
artistatbl <jdaly2991@gmail.com>
2025-04-29 16:52:18 +0100

    chore: upt landing page

--- src/components/global/landing/preview.tsx
+++ src/components/global/landing/preview.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image';
+
 export function DashboardPreview() {
     return (
       <div className="relative  rounded-sm shadow-emerald-600 p-2 max-w-6xl mx-auto transform hover:-translate-y-1 transition-transform duration-300">
         <div className="aspect-[16/9]  relative overflow-hidden rounded-sm">
-          <img
+          <Image
             src="/preview1.png"
             alt="Compliance dashboard preview"
-            className="w-full  object-cover"
+            className="object-cover"
+            fill
+            priority
           />
         </div>
       </div>
