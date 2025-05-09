fe1e24d4b272a76138a0006c61cb373a624804d7
artistatbl <jdaly2991@gmail.com>
2025-05-09 18:26:28 +0100

    refactor: update Sentry configuration to use environment variable for DSN and adjust tracesSampleRate for consistency

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,10 +5,10 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://58af929db166ce40846c1ea9a1e0e568@o4509216830193664.ingest.us.sentry.io/4509216854769664",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
