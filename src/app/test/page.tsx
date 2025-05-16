5c31e336dbfda21159f377bb4f60733c8d01fc69
artistatbl <jdaly2991@gmail.com>
2025-05-16 17:46:27 +0100

    chore: update @mantlz/nextjs package to version 0.4.5, enhance Mantlz component styling with new class names for improved visual consistency, and clean up commented-out code for better readability

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -13,18 +13,34 @@ export default function CustomerFeedbackPage() {
      
 
         
-      <Mantlz
+      {/* <Mantlz
         formId="cmao2vgrf0007o7lfm6r82jif"
-        //colorMode='dark'
-        theme='purple'
 
-        //theme=''
-       
-    className='bg-red-500'
-       
-        //theme="neobrutalism"
+       // theme='purple'
+        className="bg-gradient-to-r from-purple-100 to-pink-100 dark:from-purple-900 dark:to-pink-900 border-2 border-purple-300 dark:border-purple-700"
+
+
         
-      /> 
+
+       
+
+       
+       
+        
+      />  */}
+      <Mantlz
+  formId="cmao2vgrf0007o7lfm6r82jif"
+  className="
+    bg-gradient-to-r from-zinc-100 to-zinc-200 dark:from-zinc-900 dark:to-zinc-800 
+    border-2 border-zinc-300 dark:border-zinc-700 backdrop-blur-3xl shadow-2xl
+    [&_button]:bg-purple-500 [&_button]:hover:bg-purple-600 [&_button]:text-white
+    [&_label]:text-purple-700 [&_label]:font-medium [&_label]:text-sm
+    [&_input]:border-purple-200 [&_input]:focus:ring-purple-500
+    [&_textarea]:border-purple-200 [&_textarea]:focus:ring-purple-500
+    [&_checkbox]:border-purple-200 [&_checkbox]:focus:ring-purple-500
+    [&_file-upload]:border-purple-200 [&_file-upload]:focus:ring-purple-500
+  "
+/>
      
       <Mantlz
 
@@ -33,6 +49,9 @@ export default function CustomerFeedbackPage() {
         usersJoinedCount={1}
         usersJoinedLabel="people have already joined"
         showUsersJoined={true}
+        className="bg-gradient-to-r from-zinc-200 via-zinc-400 to-zinc-600 dark:from-zinc-900 dark:via-zinc-800 dark:to-zinc-800 border-2 border-zinc-300 dark:border-zinc-700 mx-auto my-8 p-8 space-y-6"
+
+
 
        //redirectUrl='/feedback/thank-you'
       /> 
@@ -47,25 +66,15 @@ export default function CustomerFeedbackPage() {
       
       />
 
+      <Mantlz formId="cmaqyion30003ie048dva1ebd"
+      theme='default'
+      className="bg-purple-50 dark:bg-purple-950 shadow-purple-200 dark:shadow-purple-800 shadow-lg"
+
+      />
+
      
 
-      {/* <WaitlistForm
-        formId="cm9yc2lge0001ie0480n9d87a"
-        theme="default"
-        
-      /> */}
-
-      {/* <FeedbackForm
-        formId="cm9muq8dg0013o7ks90uz4ljx"
-        theme="neobrutalism"
-        
-      /> */}
-
-      {/* <ContactForm
-        formId="cm9mw8bku0001o7hufv90muwq"
-        theme="neobrutalism"
-        
-      /> */}
+      
 
 
       
