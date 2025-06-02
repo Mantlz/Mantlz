2c16593cf6d25ad9c19e20bd7bff66d599920343
artistatbl <jdaly2991@gmail.com>
2025-06-02 03:25:21 +0100

    style(hero-section): adjust image shadow and remove commented blur effect

--- src/components/global/landing/hero-section.tsx
+++ src/components/global/landing/hero-section.tsx
@@ -45,17 +45,17 @@ export default function HeroSection() {
                 alt="App screenshot"
                 width={1216}
                 height={721}
-                className="rounded-md  shadow-2xl ring-1 ring-zinc-900/10 dark:ring-white/10"
+                className="rounded-md  shadow-2xl shadow-orange-700 ring-1 ring-zinc-900/10 dark:ring-white/10"
               />
             </div>
           </div>
         </div>}
       </div>
 
-      <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+      {/* <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
         <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-orange-900 to-orange-900 opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
         
-      </div>
+      </div> */}
     </div>
   )
 }
