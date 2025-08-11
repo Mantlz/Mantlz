2c1dcbc57e6671d81cbe4a6c901982ae06da7f21
artistatbl <jdaly2991@gmail.com>
2025-08-11 01:28:27 +0100

    style:(ui) per-navbar border upt

--- src/components/topbar/persistent-navbar.tsx
+++ src/components/topbar/persistent-navbar.tsx
@@ -314,8 +314,8 @@ export const PersistentNavbar = memo(function PersistentNavbar() {
       className={cn(
         "fixed top-0 left-0 right-0 z-50  flex h-16 items-center justify-between backdrop-blur-sm border-b px-3 sm:px-4 md:px-4 lg:px-8 text-foreground transition-all duration-300",
         scrolled
-          ? "bg-background border-border/20 "
-          : "bg-background border-border/20"
+          ? "bg-background border-border/50 "
+          : "bg-background border-border/50"
       )}
     >
       <div className="flex items-center gap-2 md:gap-6">
